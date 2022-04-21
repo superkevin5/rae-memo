6181f66d39c607d684b4b6a3cae36b6108b517ec
superkevin5 <hlmloveaustralia@gmail.com>
2022-04-22 00:01:07 +0800

    www

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -10,191 +10,11 @@ Page({
         currentDay: util.getCurrentDay(new Date().toLocaleDateString('en-GB')),
         showCalendar: false,
         showDialog: false,
+        showUpdateEvent: false,
         calendarMinDate: new Date(2020, 0, 1).getTime(),
         calendarMaxDate: new Date(2025, 0, 1).getTime(),
         timeBlocks: util.getPageTimeBlocks(),
-        // timeBlocks:[
-        //     {
-        //         id:1,
-        //         time: '01:00',
-        //         events: [
-        //             {
-        //                 id: 111,
-        //                 timeStart: '2022-04-10T14:21:48Z',
-        //                 timeEnd: '2022-04-10T14:21:48Z',
-        //                 description: 'look after mao mao1111 '
-        //             },
-        //             {
-        //                 id: 222,
-        //                 timeStart: '2022-04-10T14:21:48Z',
-        //                 timeEnd: '2022-04-10T14:21:48Z',
-        //                 description: 'look after mao mao1111 '
-        //             },
-        //             {
-        //                 id: 333,
-        //                 timeStart: '2022-04-10T14:21:48Z',
-        //                 timeEnd: '2022-04-10T14:21:48Z',
-        //                 description: 'look after mao mao11112 '
-        //             },
-        //             {
-        //                 id: 444,
-        //                 timeStart: '2022-04-10T14:21:48Z',
-        //                 timeEnd: '2022-04-10T14:21:48Z',
-        //                 description: 'look after mao mao1111 '
-        //             },
-        //             {
-        //                 id:5555,
-        //                 timeStart: '2022-04-10T14:21:48Z',
-        //                 timeEnd: '2022-04-10T14:21:48Z',
-        //                 description: 'look after mao mao1111123123 '
-        //             },
-        //             {
-        //                 id: 6666,
-        //                 timeStart: '2022-04-10T14:21:48Z',
-        //                 timeEnd: '2022-04-10T14:21:48Z',
-        //                 description: 'look after mao mao11112123123 '
-        //             },
-        //             {
-        //                 id: 7777,
-        //                 timeStart: '2022-04-10T14:21:48Z',
-        //                 timeEnd: '2022-04-10T14:21:48Z',
-        //                 description: 'look after mao mao1111123 '
-        //             },
-        //             {
-        //                 id:8888,
-        //                 timeStart: '2022-04-10T14:21:48Z',
-        //                 timeEnd: '2022-04-10T14:21:48Z',
-        //                 description: 'look after mao mao1111123 '
-        //             },
-        //             {
-        //                 id: 888887,
-        //                 timeStart: '2022-04-10T14:21:48Z',
-        //                 timeEnd: '2022-04-10T14:21:48Z',
-        //                 description: 'look after mao mao11112123 '
-        //             }
-        //
-        //         ]
-        //     },
-        //     {
-        //         id:2,
-        //         time: '02:00'
-        //     },
-        //     {
-        //         id:3,
-        //         time: '03:00'
-        //     },
-        //     {
-        //         id:4,
-        //         time: '04:00'
-        //     },
-        //     {
-        //         id:5,
-        //         time: '05:00'
-        //     },
-        //     {
-        //         id:6,
-        //         time: '06:00'
-        //     },
-        //     {
-        //         id:7,
-        //         time: '07:00'
-        //     },
-        //     {
-        //         id:8,
-        //         time: '08:00',
-        //         events: [
-        //             {
-        //               id: 1,
-        //               timeStart: '',
-        //               timeEnd: '',
-        //               description: 'look after mao mao '
-        //             },
-        //             {
-        //                 id: 2,
-        //                 timeStart: '',
-        //                 timeEnd: '',
-        //                 description: 'look after mao mao2 '
-        //             },
-        //             {
-        //                 id: 3,
-        //                 timeStart: '',
-        //                 timeEnd: '',
-        //                 description: 'look after mao mao3 '
-        //             },
-        //
-        //
-        //         ]
-        //     },
-        //     {
-        //         id:9,
-        //         time: '09:00'
-        //     },
-        //     {
-        //         id:10,
-        //         time: '10:00'
-        //     },
-        //     {
-        //         id:11,
-        //         time: '11:00'
-        //     },
-        //     {
-        //         id:12,
-        //         time: '12:00'
-        //     },
-        //     {
-        //         id:13,
-        //         time: '13:00'
-        //     },
-        //     {
-        //         id:14,
-        //         time: '14:00'
-        //     },
-        //     {
-        //         id:15,
-        //         time: '15:00'
-        //     },
-        //     {
-        //         id:16,
-        //         time: '16:00'
-        //     },
-        //     {
-        //         id:17,
-        //         time: '17:00'
-        //     },
-        //     {
-        //         id:18,
-        //         time: '18:00'
-        //     },
-        //     {
-        //         id:19,
-        //         time: '19:00'
-        //     },
-        //     {
-        //         id:20,
-        //         time: '20:00'
-        //     },
-        //     {
-        //         id:21,
-        //         time: '21:00'
-        //     },
-        //     {
-        //         id:22,
-        //         time: '22:00'
-        //     },
-        //     {
-        //         id:23,
-        //         time: '23:00'
-        //     },
-        //     {
-        //         id:24,
-        //         time: '00:00'
-        //     }
-        //
-        //
-        //
-        //
-        //
-        // ],
+
         showUploadTip: false,
         powerList: [ {
             title: 'calendar',
@@ -385,7 +205,19 @@ Page({
         this.setData({
             showDialog: true
         })
-
+    },
+    onCloseUpdateEvent() {
+        this.setData({
+            showUpdateEvent: false
+        })
+    },
+    longPressEvent(e) {
+        if (e && e.currentTarget && e.currentTarget.dataset && e.currentTarget.dataset.eventId) {
+            this.setData({
+                showUpdateEvent: true,
+                eventIdInUpdating: e.currentTarget.dataset.eventId
+            })
+        }
     },
     onClickPowerInfo( e ) {
         const index = e.currentTarget.dataset.index;
